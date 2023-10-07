7684eb7e88263eab13c18cfae7ded525485ea885
BRezorX <buddharajrai104@gmail.com>
2023-10-07 15:01:38 +0530

    Gohona Files

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    
+</x-app-layout>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	Yo Yo
+ 
+
+</body>
+</html>
